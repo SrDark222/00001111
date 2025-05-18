2665e7299cb03c0f170ad95fd581b84b5aee566c
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-18 19:07:53 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loading-screen").style.display = "none";
+    document.body.style.overflow = "auto";
+  }, 3000);
+});
